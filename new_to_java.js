efb85bbec304dfdfabba0fafed3ac66265a47be3
Chidera0001 <c.anele@alustudent.com>
2023-12-09 00:21:30 +0200

    I am still learning

---
+++ new_to_java.js
@@ -0,0 +1,118 @@
+// console.log('Hello World')
+// var Me= "Dera"
+// console.log(Me)
+// var housemate = "bere"
+// console.log(Me, housemate)
+// housemate = "max"
+// console.log(Me, housemate)
+// var petDog = 'Rex'; 
+// console.log(petDog);
+// var a = 6;
+// console.log(a>5 || a>10);
+// console.log(!(a>5));
+// console.log(a>5 && a<10);
+// console.log(4-2);
+// var petHungry = true;
+// console.log("Pet is hungry: ", !petHungry);
+// console.log(22 % 5); 
+// var score = "8";
+// console.log("mid-level skills:", score>0 && score<10);
+
+
+// var age= "10"; 
+// if (age >= 65) {
+//     console.log("You get your income from your pension")
+// }
+// else if (age<65 || age>=18){
+//     console.log("Each month you get a salary")
+// }
+// else if (age<18) {
+//     console.log("You get an allowance")
+// }
+// else{
+//     console.log("The value of the age variable is not numerical")
+// }
+
+// var day= "sunday"
+// switch (day) {
+//     case "monday":
+//         console.log("do something");
+//         break;
+//     case "tuesday":
+//         console.log("do something");
+//         break;
+//     case "wednesday":
+//         console.log ("do something");
+//         break;
+//     case "thursday":
+//         console.log ("do something");
+//         break;
+//     case "friday":
+//         console.log ("do something");
+//         break;
+//     case "saturday":
+//         console.log ("do something");
+//         break;
+//     case "sunday":
+//         console.log ("do something");
+//         break;
+//     default:
+//         console.log ("sleep")    ;
+//         break;
+// } 
+
+// for (var i = 0; i <= 3; i++) {
+//     console.log("This is line ", i)
+// }
+
+// for (i=1; i<=5; i++) {
+//     console.log(i);
+// }
+// console.log('Conting completed')
+
+// var i=2018
+// while (i<=2022) {
+//     console.log(i);
+//     i++
+// }
+// console.log('Counting completed')
+
+
+// for (i=1; i<=10; i++) {
+//     if (i==1){
+//         console.log("Gold medal");
+//     }
+//     else if (i==2){
+//         console.log("Silver Medal");
+//     }
+//     else if (i==3){
+//         console.log("Bronze Medal");
+//     }
+//     else{
+//         console.log(i);
+//     }
+// }
+
+// for (i=1; i<=10; i++) {
+//     switch (i) {
+//         case 1:
+//             console.log("Gold medal");
+//             break;
+//         case 2:
+//             console.log("Silver Medal");
+//             break;
+//         case 3:
+//             console.log("Bronze Medal");
+//             break;
+//         default:
+//             console.log(i);
+//         }
+//     }
+
+
+for (i = 0; i < 2; i++) {
+    console
+    for (var j = 0; j < 3; j++) {
+        console.log("Hello");
+    }
+}
