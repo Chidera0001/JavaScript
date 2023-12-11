3e1a1ae66b99b7eac3ae8d1aedfd8c136f60b992
Chidera0001 <c.anele@alustudent.com>
2023-12-11 02:33:03 +0200

    I am getting better

--- new_to_java.js
+++ new_to_java.js
@@ -110,9 +110,131 @@
 //     }
 
 
-for (i = 0; i < 2; i++) {
-    console
-    for (var j = 0; j < 3; j++) {
-        console.log("Hello");
+// for (i = 0; i < 2; i++) {
+//     console
+//     for (var j = 0; j < 3; j++) {
+//         console.log("Hello");
+//     }
+// }
+
+// function older(age){
+//         if (age<=17) {
+//             console.log("You are a child");
+//         }
+//         else if (age<=30){
+//             console.log("you are a youth");
+//         }
+//         else{
+//             console.log("You are an adult");
+//         }
+// }
+// older(100);
+
+// function letterfinder(word, match){
+//     for (i=0; i<word.length; i++){
+//         if (word[i]==match){
+//             console.log("found the", match, "at", i);
+//         }
+//         else{
+//             console.log("--No match found at", i)
+//         }
+//     }
+// }
+// letterfinder("test", "t");
+
+// var housemates ={}
+// housemates.max = 24;
+// housemates.damian= 23;
+// housemates.dera = 19;
+// housemates.chris = 19;
+// console.log(housemates)
+// housemates.dera = 20;
+// console.log(housemates)
+
+// var housemates = {
+//     max:24,
+//     damian: 23,
+//     dera: 19,
+//     chris: 19
+// }
+// console.log(housemates)
+
+// var names =['dera', 'me', 'us'];
+// console.log(names.length);
+// console.log(names[0]);
+
+// var greet = "Hello";
+// console.log(greet.length);
+
+// var clothes =[];
+// clothes.push("shirt");
+// clothes.push("pants");
+// clothes.push("shoes");
+// clothes.push("Necklace");
+// clothes.push("socks");
+// clothes.pop("socks");
+// clothes.push("earrings");
+// console.log(clothes[2]);
+// var favCar ={}
+// favCar.color ='green';
+// favCar.convertible =true;
+// console.log(favCar);
+
+// try{
+//     console.log(c+d);
+// }catch(err){
+//     console.log("Error:", err.message);
+// }
+// console.log('I am a guy')
+
+// try{
+//     throw new SyntaxError();
+// }catch(err){
+//     console.log('error:', err.message);
+// }
+
+// function addTwoNums(a, b){
+//     try{
+//         if (typeof (a) != 'number'){
+//             throw new Error('First argument must be a number');
+//         }
+//         else if (typeof (b) != 'number'){
+//             throw new Error('Second argument must be a number');
+//         }
+//         console.log(a+b);
+//     }
+//     catch(err){
+//         console.log("Error!", err);
+//     }
+// }
+// addTwoNums(5, "5");
+// console.log("It still works");
+
+// var even = "2";
+// var number_type = Number(even)
+// console.log(typeof even);
+
+// function letterFinder(word, match) {
+//     var condition1 = typeof(word) == 'string' && word.length >= 2;
+//     var condition2 = typeof(match) == 'string' && match.length ==1;
+//     if (condition1 == true & condition2 == true){
+//         for(var i = 0; i < word.length; i++) {
+//             if(word[i] == match) {
+//                 //if the current character at position i in the word is equal to the match
+//                 console.log('Found the', match, 'at', i)
+//             } else {
+//                 console.log('---No match found at', i)
+//             }
+//         }
+//     }
+//     else{
+//         console.log("Please pass correct arguments to the function.")
+//     }
+// }
+// letterFinder("cat", "c");
+
+try {
+    Number(5).toPrecision(300)
+    } catch(e) {
+    console.log("There was an error")
     }
-}
