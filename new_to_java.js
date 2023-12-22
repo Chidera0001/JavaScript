2dd0b57f8a695ff8e9329c56baaae65dcfa615b0
Chidera0001 <c.anele@alustudent.com>
2023-12-22 20:47:34 +0200

    I don suffer

--- new_to_java.js
+++ new_to_java.js
@@ -270,12 +270,85 @@
 // console.log(type(num(5)));
 
 //creating an object
-var virtualPet = {
-  sleepy: true,
-  nap: function () {
-    this.sleepy = false;
-  },
-};
-console.log(virtualPet.sleepy); // true
-virtualPet.nap();
-console.log(virtualPet.sleepy); // false
+
+// var virtualPet = {
+//   sleepy: true,
+//   nap: function () {
+//     this.sleepy = false;
+//   },
+// };
+// console.log(virtualPet.sleepy); // true
+// virtualPet.nap();
+// console.log(virtualPet.sleepy); // false
+
+// function addTwoNums(a, b) {
+//   console.log(a + b);
+// }
+
+// function randomNum() {
+//   return Math.floor(Math.random() * 10 + 1);
+// }
+// function specificNum() {
+//   return 42;
+// }
+
+// var useRandom = true;
+
+// var getNumber;
+
+// if (useRandom) {
+//   getNumber = randomNum;
+// } else {
+//   getNumber = specificNum;
+// }
+
+// addTwoNums(getNumber(), getNumber());
+
+// Task 1: Build a function-based console log message generator
+
+var message = undefined;
+var style = undefined;
+
+function consoleStyler(color, background, fontSize, txt) {
+  var message = "%c" + txt;
+
+  var style = `color: ${color};`;
+
+  style += `background: ${background};`;
+
+  style += `font-size: ${fontSize};`;
+
+  console.log(message, style);
+}
+
+// Task 2: Build another console log message generator
+
+function celebrateStyler(reason) {
+  var fontStyle = "color: tomato; font-size: 50px";
+
+  if (reason == "birthday") {
+    console.log(`%cHappy birthday`, fontStyle);
+  } else if (reason == "champions") {
+    console.log(`%cCongrats on the title!`, fontStyle);
+  } else {
+    console.log(message, style);
+  }
+}
+
+// Task 3: Run both the consoleStyler and the celebrateStyler functions
+
+consoleStyler("#1d5c63", "#ede6db", "40px", "Congrats!");
+
+celebrateStyler("birthday");
+
+// Task 4: Insert a congratulatory and custom message
+
+function styleAndCelebrate() {
+  consoleStyler("#ef7c8e", "#fae8e0", "30px", "You made it!");
+
+  celebrateStyler("champions");
+}
+
+// Call styleAndCelebrate
+
+styleAndCelebrate();
