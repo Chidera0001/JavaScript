d3f4dd99b3f39dbd2ecf6323f32aec3106cc135b
Chidera0001 <c.anele@alustudent.com>
2023-12-27 16:53:47 +0200

    task

--- new_to_java.js
+++ new_to_java.js
@@ -306,49 +306,69 @@
 
 // Task 1: Build a function-based console log message generator
 
-var message = undefined;
-var style = undefined;
+// var message = undefined;
+// var style = undefined;
 
-function consoleStyler(color, background, fontSize, txt) {
-  var message = "%c" + txt;
+// function consoleStyler(color, background, fontSize, txt) {
+//   var message = "%c" + txt;
 
-  var style = `color: ${color};`;
+//   var style = `color: ${color};`;
 
-  style += `background: ${background};`;
+//   style += `background: ${background};`;
 
-  style += `font-size: ${fontSize};`;
+//   style += `font-size: ${fontSize};`;
 
-  console.log(message, style);
+//   console.log(message, style);
+// }
+
+// // Task 2: Build another console log message generator
+
+// function celebrateStyler(reason) {
+//   var fontStyle = "color: tomato; font-size: 50px";
+
+//   if (reason == "birthday") {
+//     console.log(`%cHappy birthday`, fontStyle);
+//   } else if (reason == "champions") {
+//     console.log(`%cCongrats on the title!`, fontStyle);
+//   } else {
+//     console.log(message, style);
+//   }
+// }
+
+// // Task 3: Run both the consoleStyler and the celebrateStyler functions
+
+// consoleStyler("#1d5c63", "#ede6db", "40px", "Congrats!");
+
+// celebrateStyler("birthday");
+
+// // Task 4: Insert a congratulatory and custom message
+
+// function styleAndCelebrate() {
+//   consoleStyler("#ef7c8e", "#fae8e0", "30px", "You made it!");
+
+//   celebrateStyler("champions");
+// }
+
+// // Call styleAndCelebrate
+
+// styleAndCelebrate();
+
+// var globalVar = 77;
+
+// function scopeTest() {
+//   var localVar = 88;
+// }
+
+// console.log(localVar);
+
+function meal(animal) {
+  animal.food = animal.food + 10;
 }
 
-// Task 2: Build another console log message generator
+var dog = {
+  food: 10,
+};
+meal(dog);
+meal(dog);
 
-function celebrateStyler(reason) {
-  var fontStyle = "color: tomato; font-size: 50px";
-
-  if (reason == "birthday") {
-    console.log(`%cHappy birthday`, fontStyle);
-  } else if (reason == "champions") {
-    console.log(`%cCongrats on the title!`, fontStyle);
-  } else {
-    console.log(message, style);
-  }
-}
-
-// Task 3: Run both the consoleStyler and the celebrateStyler functions
-
-consoleStyler("#1d5c63", "#ede6db", "40px", "Congrats!");
-
-celebrateStyler("birthday");
-
-// Task 4: Insert a congratulatory and custom message
-
-function styleAndCelebrate() {
-  consoleStyler("#ef7c8e", "#fae8e0", "30px", "You made it!");
-
-  celebrateStyler("champions");
-}
-
-// Call styleAndCelebrate
-
-styleAndCelebrate();
+console.log(dog.food);
