0b46dc8a9756e213537def997cd34918bc2c7e2e
Chidera0001 <c.anele@alustudent.com>
2024-01-04 10:04:21 +0200

    java

--- OOP.js
+++ OOP.js
@@ -20,8 +20,6 @@
 // baldEagle.useWings(); // "Flying! Barely flapping!"
 // kingPenguin.useWings(); // "Diving!"
 
-
-
 /* Constructors in JavaScript*/
 // function Icecream(flavor) {
 //   this.flavor = function () {
@@ -36,8 +34,6 @@
 // let appleIcecream = new Icecream("apple");
 // kiwiIcecream.flavor();
 
-
-
 /* Inheritanve in JavaScript*/
 // var man = {
 //   legs: true,
@@ -49,5 +45,21 @@
 // console.log("slimbro has beard:", slimbro);
 // slimbro.beard = false;
 // console.log("slimbro has beard:", slimbro.beard);
-
-
+class WithDefaultParams {
+  constructor(num1 = 1, num2 = 2, num3 = 3, string1 = "Result:", bool1 = true) {
+    this.num1 = num1;
+    this.num2 = num2;
+    this.num3 = num3;
+    this.string1 = string1;
+    this.bool1 = bool1;
+  }
+  calculate() {
+    if (this.bool1) {
+      console.log(this.string1, this.num1 + this.num2 + this.num3);
+      return;
+    }
+    return "The value of bool1 is incorrect";
+  }
+}
+var better = new WithDefaultParams();
+better.calculate();
