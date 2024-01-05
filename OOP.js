df592fcca1829c1bbf8c5036fcd997ba1d67c77d
Chidera0001 <c.anele@alustudent.com>
2024-01-05 13:38:53 +0200

    JavaScript

--- OOP.js
+++ OOP.js
@@ -239,12 +239,18 @@
 // console.log(shoppingcart);
 
 // /*Here's how to copy an object into a completely separate object, using the spread operator*/
-const car1 = {
-  speed: 200,
-  color: "yellow",
-};
-const car2 = { ...car1 };
+// const car1 = {
+//   speed: 200,
+//   color: "yellow",
+// };
+// const car2 = { ...car1 };
 
-car1.speed = 201;
+// car1.speed = 201;
 
-console.log(car1.speed, car2.speed);
+// console.log(car1.speed, car2.speed);
+
+/*You can copy an array into a completely separate array, also using the spread operator, like this:*/
+const fruits1 = ["apples", "pears"];
+const fruits2 = [...fruits1];
+fruits1.pop();
+console.log(fruits1, "not", fruits2);
