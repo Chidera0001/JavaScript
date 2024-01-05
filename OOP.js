e6d67c7921f156eb3175391f459e3fe30c1a6dba
Chidera0001 <c.anele@alustudent.com>
2024-01-05 13:32:28 +0200

    yes

--- OOP.js
+++ OOP.js
@@ -231,9 +231,20 @@
 // }
 
 /* Rest operator in functions */
-function addTaxToPrices(taxRate, ...itemsBought) {
-  return itemsBought.map((item) => taxRate * item);
-}
+// function addTaxToPrices(taxRate, ...itemsBought) {
+//   return itemsBought.map((item) => taxRate * item);
+// }
 
-let shoppingcart = addTaxToPrices(1.1, 46, 89, 35, 79);
-console.log(shoppingcart);
+// let shoppingcart = addTaxToPrices(1.1, 46, 89, 35, 79);
+// console.log(shoppingcart);
+
+// /*Here's how to copy an object into a completely separate object, using the spread operator*/
+const car1 = {
+  speed: 200,
+  color: "yellow",
+};
+const car2 = { ...car1 };
+
+car1.speed = 201;
+
+console.log(car1.speed, car2.speed);
