9ed466c8b6f57a8625f060d0008ff53d9d17afd0
Chidera0001 <c.anele@alustudent.com>
2024-01-06 09:16:51 +0200

    coe

--- JavaScript-in-browsers.js
+++ JavaScript-in-browsers.js
@@ -48,3 +48,26 @@ function scopeTest() {
 
 var x = 33;
 scopeTest();
+
+class Cake {
+  constructor(lyr) {
+    this.layers = lyr;
+  }
+
+  getLayers() {
+    return this.layers;
+  }
+}
+
+class WeddingCake extends Cake {
+  constructor() {
+    super(2);
+  }
+
+  getLayers() {
+    return super.getLayers() * 5;
+  }
+}
+
+var result = new WeddingCake();
+console.log(result.getLayers());
